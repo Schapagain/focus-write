0e6b5b66e1b93b18bce8385229d1f99f27bde20d
Sandesh <chapagainsandesh88@gmail.com>
2021-04-29 12:07:26 -0400

    refactor

---
+++ src/utils/index.js
@@ -0,0 +1,31 @@
+import adjectives from "./adjectives";
+import nouns from "./nouns";
+
+export function saveToLocalStorage(obj) {
+  console.log("saving to local::", obj);
+  Object.keys(obj).forEach((key) =>
+    localStorage.setItem([key], JSON.stringify(obj[key]))
+  );
+}
+
+export function getFromLocalStorage(key) {
+  const val = localStorage.getItem(key);
+  return val && JSON.parse(val);
+}
+
+export function getRandomAdjective() {
+  return capitalize(adjectives[getRandomInt(adjectives.length)]);
+}
+
+export function getRandomNoun() {
+  return capitalize(nouns[getRandomInt(nouns.length)]);
+}
+
+export function getRandomInt(max = 10000) {
+  return Math.floor(Math.random() * max);
+}
+
+export function capitalize(s) {
+  if (typeof s !== "string") return "";
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
