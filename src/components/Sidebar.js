7d3798ee768c5527e6006b3ccd1dfc571cccca48
Sandesh <chapagainsandesh88@gmail.com>
2021-04-29 11:50:32 -0400

    change sidebar layout

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,19 +1,28 @@
 import classNames from "classnames";
 import React, { useContext, useState } from "react";
-import { DocumentsContext } from "../context/DocumentsContext";
 import { AiOutlineRightCircle } from "react-icons/ai";
+import { MdAddCircleOutline, MdRemoveCircleOutline } from "react-icons/md";
+
+import { DocumentsContext } from "../context/DocumentsContext";
+import IconButton from "./utils/IconButton";
+import RemoveModal from "./RemoveModal";
 
 export default function Sidebar({}) {
   const [isOpen, setOpen] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-  const { documents, currentDocument, changeCurrentDocument } = useContext(
-    DocumentsContext
-  );
+  const {
+    documents,
+    currentDocument,
+    changeCurrentDocument,
+    addDocument,
+    removeDocument,
+  } = useContext(DocumentsContext);
 
   const classes = classNames(
-    "absolute left-0 top-1/4 bg-theme dark:bg-theme-dark",
-    "h-52 p-3 rounded-lg oveflow-hidden",
-    "transition-width duration-500 ease pr-10",
+    "h-60 p-3 rounded-lg oveflow-hidden",
+    "transition-width duration-500 ease",
+    "flex flex-col",
     {
       "w-0": !isOpen,
       "w-48": isOpen,
@@ -21,33 +30,61 @@ export default function Sidebar({}) {
   );
 
   return (
-    <div className={classes}>
-      <div className="relative">
+    <div className="flex absolute bg-theme pr-2 rounded-r-lg transition-normal dark:bg-theme-dark left-0 top-1/4">
+      <div className={classes}>
         <ul
           className="h-full overflow-y-auto cursor-pointer no-scrollbar"
           role="tablist"
         >
           {documents &&
-            Object.keys(documents).map((id) => (
+            documents.map((document) => (
               <li
                 role="tab"
-                key={id}
+                key={document.id}
                 className={`${
-                  currentDocument.id == id ? "bg-gray-200" : ""
-                } py-1 pl-2 rounded-r-xl`}
-                onClick={() => changeCurrentDocument(documents[id])}
+                  currentDocument.id == document.id ? "bg-gray-200" : ""
+                } py-1 pl-2 truncate animate-fade-in rounded-r-xl`}
+                onClick={() => changeCurrentDocument(document)}
               >
-                {documents[id].title}
+                {document.title}
               </li>
             ))}
         </ul>
+      </div>
+      <div className="flex flex-col justify-between items-center">
+        <IconButton onClick={addDocument}>
+          <MdAddCircleOutline
+            className={`text-theme-dark transition-normal transform text-xl dark:text-white ${
+              isOpen ? " rotate-360 " : ""
+            }`}
+          />
+        </IconButton>
         <AiOutlineRightCircle
           onClick={() => setOpen(!isOpen)}
-          className={`text-4xl cursor-pointer text-white absolute -right-10 top-1/2 -translate-y-1/2 transition-normal transform ${
-            isOpen ? "rotate-180" : ""
+          className={`text-4xl cursor-pointer text-white transform transition-normal ${
+            isOpen
+              ? " hover:-translate-x-1 rotate-180 "
+              : " hover:translate-x-1 "
           }`}
         />
+        <IconButton>
+          <MdRemoveCircleOutline
+            onClick={() => setShowModal(true)}
+            className={`text-theme-dark transition-normal transform text-xl dark:text-white ${
+              isOpen ? " rotate-360 " : ""
+            }`}
+          />
+        </IconButton>
       </div>
+      {showModal && (
+        <RemoveModal
+          onDelete={() => {
+            removeDocument(currentDocument.id);
+            setShowModal(false);
+          }}
+          onClose={() => setShowModal(false)}
+        />
+      )}
     </div>
   );
 }
